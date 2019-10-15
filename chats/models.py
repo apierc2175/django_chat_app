7da13eaf24cf1fa4b002d0993405d9267d32e8d4
apierc2175 <aidanwpierce@gmail.com>
2019-10-15 15:50:23 -0400

    can add topic names using admin

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('chats:index')
--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from django.views.generic.edit import CreateView
+#reverse lazy allows it to fully complete before executing
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
+from .models import Topic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-# Create your views here.
+def index(request):
+    chat_list = Topic.objects.all()
+
+    context = {
+        'chat_list': chat_list,
+    }
+
+    return render(request, 'chats/index.html', context)
